7142164952924f5b8ff008e4e2ad98cb24b68a90
Arroyo <gabrielarroyoc18@gmail.com>
2023-08-22 00:05:10 -0300

    Initial commit

---
+++ nextapp-agency/components/Header/index.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+const nav = [
+  {
+    href: "/",
+    label: "About",
+  },
+  {
+    href: "/resume",
+    label: "Resume",
+  },
+];
+
+export const Header = () => {
+  return (
+    <header className="max-w-screen-lg mx-auto px-4 py-4 flex items-center justify-between">
+      <Link href="/">
+        <div className="text-4xl font-bold text-zinc-900 select-none">⚡</div>
+      </Link>
+
+      <div className="flex gap-4 items-center">
+        <nav className="flex gap-4 items-center ">
+          {nav.map((item) => {
+            const { href, label } = item;
+
+            return (
+              <Link href={href} key={href}>
+                <span className={`text-sm  text-zinc-700`}>{label}</span>
+              </Link>
+            );
+          })}
+        </nav>
+
+      </div>
+    </header>
+  );
+};
