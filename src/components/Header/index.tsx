83f8d6cf109d9bbe766d0189c2faef59dfd35857
LuizGarbini <garbasneto@gmail.com>
2023-08-23 23:10:03 -0300

    header styles update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,63 +1,70 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
+import { User } from 'lucide-react'
 
 const nav = [
   {
-    href: "/",
-    label: "Home",
+    href: '/',
+    label: 'Home',
   },
   {
-    href: "/",
-    label: "Cursos e Programas",
+    href: '/',
+    label: 'Cursos e Programas',
   },
   {
-    href: "/",
-    label: "Eventos",
+    href: '/',
+    label: 'Eventos',
   },
   {
-    href: "/",
-    label: "Blog",
+    href: '/',
+    label: 'Blog',
   },
   {
-    href: "/",
-    label: "Sobre nós",
+    href: '/',
+    label: 'Sobre nós',
   },
   {
-    href: "/",
-    label: "Para empresas",
+    href: '/',
+    label: 'Para empresas',
   },
-];
+]
 
 export const Header = () => {
   return (
-    <header className="max-w-screen-lg mx-auto px-4 py-4 flex items-center justify-between">
+    <header className="gray-500 mx-auto flex w-full items-center justify-between border-b border-gray-700 px-10 py-6">
       <Link href="/">
-        <div className="text-4xl font-bold text-white select-none"><Image width={200} height={40} src="logo-rocket.svg" alt="Logo" /> </div>
+        <div className="select-none text-4xl font-bold text-white">
+          <Image width={200} height={40} src="logo-rocket.svg" alt="Logo" />{' '}
+        </div>
       </Link>
 
       <div className="">
-        <nav className="flex justify-between">
+        <nav className="flex justify-between text-gray-200">
           {nav.map((item) => {
-            const { href, label } = item;
+            const { href, label } = item
 
             return (
               <Link href={href} key={href}>
-                <span className={`p-3 text-sm text-linksNavHeader transition duration-300 ease-in-out cursor-pointer`}>{label}</span>
+                <span
+                  className={`text-linksNavHeader cursor-pointer p-3 text-sm transition duration-300 ease-in-out`}
+                >
+                  {label}
+                </span>
               </Link>
-            );
+            )
           })}
         </nav>
-
       </div>
 
-      <div className="flex justify-between gap-6 text-sm">
-          <a href="/" className="">
-            ENTRAR
-          </a>
-          <a href="/">
-            ENTRAR
-          </a>
+      <div className="flex items-center justify-between text-sm">
+        <a className="flex items-center gap-2 px-6  py-3" href="/">
+          <User className="stroke-rocket-500" size={20} />
+          ENTRAR
+        </a>
+        <a className="rounded-md border border-rocket-500 px-6 py-3 " href="/">
+          CRIAR CONTA
+        </a>
       </div>
     </header>
-  );
-};
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,14 +9,27 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'purpleRocket':'#996DFF',
-        'linksNavHeader': '#C4C4CC',
-
+        purpleRocket: '#996DFF',
+        gray: {
+          200: '#C4C4CC',
+          300: '#C4C4CC',
+          700: '#29292E',
+        },
+        rocket: {
+          50: '#f3eefc',
+          100: '#d8cbf7',
+          200: '#c6b2f3',
+          300: '#ab8eee',
+          400: '#9b79ea',
+          500: '#8257e5',
+          600: '#764fd0',
+          700: '#5c3ea3',
+          800: '#48307e',
+          900: '#372560',
+        },
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      borderRadius: {
+        md: '5px',
       },
     },
   },
