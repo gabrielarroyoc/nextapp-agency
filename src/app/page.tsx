b8f402f08d6f056265b562ffddae45776e704814
Arroyo <gabrielarroyoc18@gmail.com>
2023-08-30 19:46:12 -0300

    feat: guide table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,146 +50,216 @@ export default function Home() {
         />
       </section>
       <section>
-        <div>
-          <h3 className="text-center text-base font-medium  text-purpleRocket">
-            EMPRESAS QUE JÁ LEVARAM SEUS DEVS PARA O <br />
-            PRÓXIMO NÍVEL
+        <div className="items-center py-20 text-center">
+          <h3 className="text-center text-base font-medium uppercase   text-purpleRocket">
+            empresas que já levaram seus devs para o <br />
+            próximo nível
           </h3>
           <div className="hidden xl:my-[60px] xl:flex xl:flex-col xl:gap-[60px]">
             <div className="flex justify-center gap-10">
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/mercado-livre.svg'}
-                width={150}
+                width={120}
                 alt="mercado livre"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/vtex.svg'}
-                width={150}
+                width={80}
                 alt="vtex"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/globo.svg'}
-                width={150}
+                width={110}
                 alt="globo"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/ifood.svg'}
-                width={150}
+                width={55}
                 alt="ifood"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/itau.svg'}
-                width={150}
+                width={35}
                 alt="itau"
-                height={150}
+                height={35}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/stone.svg'}
-                width={150}
+                width={70}
                 alt="stone"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/compass-uol.svg'}
-                width={150}
+                width={180}
                 alt="compass-uol"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/totvs.svg'}
-                width={150}
+                width={100}
                 alt="totvs"
-                height={150}
+                height={30}
               />
             </div>
             <div className="flex justify-center gap-10">
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/accenture.svg'}
-                width={150}
+                width={115}
                 alt="accenture"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/twilio.svg'}
-                width={150}
+                width={90}
                 alt="twilio"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/riachuelo.svg'}
-                width={150}
+                width={140}
                 alt="riachuelo"
-                height={150}
+                height={16}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/suzano.svg'}
-                width={150}
+                width={130}
                 alt="suzano"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/datadog.svg'}
-                width={150}
+                width={115}
                 alt="datadog"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/invillia.svg'}
-                width={150}
+                width={90}
                 alt="invillia"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/fcamara.svg'}
-                width={150}
+                width={100}
                 alt="fcamara"
-                height={150}
+                height={30}
               />
             </div>
             <div className="flex justify-center gap-10">
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/thoughtworks.svg'}
                 width={150}
                 alt="thoughtworks"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/banco-do-brasil.svg'}
-                width={150}
+                width={180}
                 alt="banco-do-brasil"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/ambev-tech.svg'}
-                width={150}
+                width={175}
                 alt="ambev-tech"
-                height={150}
+                height={30}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/radix.svg'}
-                width={150}
+                width={60}
                 alt="radix"
-                height={150}
+                height={20}
               />
               <Image
+                className="transition duration-200 ease-in-out hover:scale-110"
                 src={'/companies/luizalabs.svg'}
-                width={150}
+                width={100}
                 alt="luizalabs"
-                height={150}
+                height={40}
               />
             </div>
           </div>
+          <a
+            className="rounded-md border border-rocket-500 bg-rocket-500 px-8 py-5 uppercase"
+            href="/"
+          >
+            quero ser um parceiro
+          </a>
         </div>
-        <a
-          className="flex w-full max-w-[233px] rounded-md border border-rocket-500 bg-rocket-500 px-6 py-4"
-          href="/"
-        >
-          QUERO SABER MAIS
-        </a>
+      </section>
+      <section>
+        <div>
+          <h3 className="text-center text-base font-medium  text-purpleRocket">
+            SIMPLES E FÁCIL
+          </h3>
+          <h3 className="text-center text-[40px] text-base font-medium text-white">
+            Entenda como funciona
+          </h3>
+          <div className="mt-7 border border-b-0 border-gray-600 text-white md:grid md:grid-cols-3 md:divide-x">
+            <div className="flex flex-col border-b border-gray-600 p-6 hover:bg-gray-800">
+              <span className="text-2xl text-gray-400">01</span>
+              <span className="pt-6 text-xl text-gray-100">
+                Cadastre sua empresa através do formulário clicando aqui
+              </span>
+            </div>
+            <div className="flex flex-col border-b border-gray-600 p-6 hover:bg-gray-800">
+              <span className="text-2xl text-gray-400">02</span>
+              <span className="pt-6 text-xl text-gray-100">
+                Nosso time entra em contato para agendar uma demonstração
+              </span>
+            </div>
+            <div className="flex flex-col border-b border-gray-600 p-6 hover:bg-gray-800">
+              <span className="text-2xl text-gray-400">03</span>
+              <span className="pt-6 text-xl text-gray-100">
+                Faça a assinatura de nossa plataforma com condições especiais
+              </span>
+            </div>
+            <div className="flex flex-col border-b border-gray-600 p-6 hover:bg-gray-800">
+              <span className="text-2xl text-gray-400">04</span>
+              <span className="pt-6 text-xl text-gray-100">
+                Gerencie as licenças e acessos para seu time
+              </span>
+            </div>
+            <div className="flex flex-col border-b border-gray-600 p-6 hover:bg-gray-800">
+              <span className="text-2xl text-gray-400">05</span>
+              <span className="pt-6 text-xl text-gray-100">
+                Acompanhe o andamento do seu time através de dashboards e
+                relatórios
+              </span>
+            </div>
+            <div className="flex flex-col border-b border-gray-600 p-6 hover:bg-gray-800">
+              <span className="text-2xl text-gray-400">06</span>
+              <span className="pt-6 text-xl text-gray-100">
+                Leve os conhecimentos do seu time para o próximo nível, do zero
+                ao avançado.
+              </span>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   )
